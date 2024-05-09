60f13ae9b294256a0d971d595df633a7798febe8
volodeead <35310359+volodeead@users.noreply.github.com>
2024-05-09 18:01:41 +0300

    09.05.2024

---
+++ LanguageCourseService/Course.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace LanguageCourseService
+{
+    [DataContract(Namespace = "")]
+    public class Course
+    {
+        [DataMember]
+        public int CourseID { get; set; }
+
+        [DataMember]
+        public string CourseName { get; set; }
+
+        [DataMember]
+        public decimal Price { get; set; }
+
+        [DataMember]
+        public DateTime StartDate { get; set; }
+    }
+
+}
---
+++ LanguageCourseService/LanguageCourseService.svc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace LanguageCourseService
+{
+
+        public class LanguageCourseService : ILanguageCourseService
+        {
+            public List<Course> GetCourses()
+            {
+                return PopulateCourseData();
+            }
+
+            private List<Course> PopulateCourseData()
+            {
+                // Тут ви створите список курсів, як ми описали раніше
+                List<Course> listCourses = new List<Course>();
+                listCourses.Add(new Course { CourseID = 1, CourseName = "English Beginner", Price = 100.00m, StartDate = new DateTime(2024, 1, 1) });
+                listCourses.Add(new Course { CourseID = 2, CourseName = "French Intermediate", Price = 150.00m, StartDate = new DateTime(2024, 2, 1) });
+                return listCourses;
+            }
+        }
+
+}
